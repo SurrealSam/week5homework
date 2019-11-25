df9843d631da4de2bd677ec0271fc26c2cb58675
Sammye Jones <sajones@ucdavis.edu>
2019-11-24 19:37:35 -0800

    working input fields and added content to md file

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,14 @@
+//a bunch of hooks and shortcuts
+var form9 = document.getElementById("form9");
+var form10 = document.getElementById("form10");
+var form11 = document.getElementById("form11");
+var form12 = document.getElementById("form12");
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
+var form3 = document.getElementById("form3");
+var form4 = document.getElementById("form4");
+var form5 = document.getElementById("form5");
+
 var button9 = document.getElementById("button9");
 var button10 = document.getElementById("button10");
 var button11 = document.getElementById("button11");
@@ -7,3 +18,78 @@ var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var button4 = document.getElementById("button4");
 var button5 = document.getElementById("button5");
+
+var info9 = form9.value;
+var info10 = form10.value;
+var inf11 = form11.value;
+var info12 = form12.value;
+var info1 = form1.value;
+var info2 = form2.value;
+var info3 = form3.value;
+var info4 = form4.value;
+var info5 = form5.value;
+
+//event listeners for buttons to set local storage based on input fields
+button9.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form9.value);
+    window.localStorage.setItem('info9', JSON.stringify(form9.value));
+});
+button10.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form10.value);
+    window.localStorage.setItem('info10', JSON.stringify(form10.value));
+});
+
+button11.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form11.value);
+    window.localStorage.setItem('info11', JSON.stringify(form11.value));
+});
+button12.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form12.value);
+    window.localStorage.setItem('info12', JSON.stringify(form12.value));
+});
+button1.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form1.value);
+    window.localStorage.setItem('info1', JSON.stringify(form1.value));
+});
+button2.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form2.value);
+    window.localStorage.setItem('info2', JSON.stringify(form2.value));
+});
+button3.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form3.value);
+    window.localStorage.setItem('info3', JSON.stringify(form3.value));
+});
+button4.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form4.value);
+    window.localStorage.setItem('info4', JSON.stringify(form4.value));
+});
+button5.addEventListener("click", function () {
+    event.preventDefault();
+    console.log(form5.value);
+    window.localStorage.setItem('info5', JSON.stringify(form5.value));
+});
+
+//loads saved local storage items to input fields
+$(document).ready(function(){
+    
+    form9.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info9'))) + " \"\ ";
+    form10.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info10'))) + " \"\ ";
+    form11.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info11'))) + " \"\ ";
+    form12.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info12'))) + " \"\ ";
+    form1.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info1'))) + " \"\ ";
+    form2.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info2'))) + " \"\ ";
+    form3.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info3'))) + " \"\ ";
+    form4.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info4'))) + " \"\ ";
+    form5.setAttribute('placeholder', "" + JSON.parse(window.localStorage.getItem('info5'))) + " \"\ ";
+
+    
+    });
+  
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # week5homework
 
-# Day Planner
+## Day Planner
+A list of times during the workday with fields for inputs and buttons to save those fields for later use.
+![Alt text](./Assets/DayPlanner.PNG?raw=true "DayPlannerPreview")
